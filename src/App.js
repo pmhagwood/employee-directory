78808939b54c424ca088a439c067bfbf3f95a0ef
Paul Hagwood <SubtleKnife@Pauls-MacBook-Pro.local>
2021-01-23 10:02:15 -0500

    added initial navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+    <>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Employee Directory</h1>
+          <p className="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        </div>
+      </div>
+      <div>
+        <Navbar />
+      </div>
+    </>
+    // </Router>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+    <div className="col-4">
+        Please FREAKING!!! WORK!!!
+    </div>
+  </nav>
+</>
+  );
+}
+
+export default Navbar;
